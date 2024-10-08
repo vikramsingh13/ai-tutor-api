0907f09e5da257c025996259bdbd8af0160f88ef
vikramsingh13 <vikramsingh.webdev@gmail.com>
2024-10-07 22:38:47 -0400

    updated template and fixed json_example

--- ai_logistics/openai/client.py
+++ ai_logistics/openai/client.py
@@ -25,7 +25,6 @@ def get_chat_response_with_prompt(user_prompt: str, model: str = GPT_MODEL):
   """
   get_chat_response_with_prompt takes a string param prompt and a string param model which is set to the module's GPT_MODEL constant by default. 
   """
-
   # Construct the prompt from the user_prompt and template.
   prompt = construct_prompt_from_no_context_template(no_context_template, json_example, user_prompt)
 
@@ -45,53 +44,15 @@ def get_chat_response_with_prompt(user_prompt: str, model: str = GPT_MODEL):
 
   # Return the message content from the first choice of the chat completion.
   return chat_completion.choices[0].message.content
+  
 
 
 # TODO: refactor these to utils, templates, etc.
 context_template = "Context information is below.\n---------------------\n{}\n---------------------\nCourse content example JSON:\n---------------------\n{}\n---------------------\nGiven the context information, example JSON structure, and not using prior knowledge, answer the query with a course content JSON. Answer should only contain a JSON output and nothing else.\nQuery: {}\nAnswer:\n"
 
-no_context_template = "Your task is to help create a course from any given context and user query. Course content example JSON structure:\n---------------------\n{}\n---------------------\nGiven the example JSON structure, answer the user query with a course content JSON with 5 modules and each module containing 3 sub modules. The sub module content should have enough text to help the user learn the concept mentioned in the heading. Answer should only contain a JSON output with course content related to the query and nothing else.\nQuery: {}\nAnswer:\n"
+no_context_template = "Your task is to help create a course from any given context and user query. Course content example JSON structure:\n---------------------\n{}\n---------------------\nGiven the example JSON structure, answer the user query with a course content JSON with 5 modules and each module containing 3 sub modules. The sub module content should have enough text to help the user learn the concept mentioned in the heading. Answer should only contain a JSON output with course content related to the query and nothing else. Make sure the answer JSON is one long string with no newline, tab, etc, characters. \nQuery: {}\nAnswer:\n"
 
-json_example = """{
-  courseTopic: "Computers",
-  currentModuleIndex: 1,
-  modules: {
-    1: {
-      title: "Title 1",
-      description:
-        "description 1",
-      subModules: {
-        1: {
-          heading: "Sub Module 1",
-          content:
-            "content",
-        },
-        2: {
-          heading: "Sub Module 2",
-          content:
-            "content",
-        },
-      },
-    },
-    2: {
-      title: "Title 1",
-      description:
-        "description 1",
-      subModules: {
-        1: {
-          heading: "Sub Module 1",
-          content:
-            "content",
-        },
-        2: {
-          heading: "Sub Module 2",
-          content:
-            "content",
-        },
-      },
-    },
-  },
-};"""
+json_example = """{"courseTopic":"Computers","modules":{"1":{"title":"Title 1","description":"description 1","subModules":{"1":{"heading":"Sub Module 1","content":"content"},"2":{"heading":"Sub Module 2","content":"content"}}},"2":{"title":"Title 1","description":"description 1","subModules":{"1":{"heading":"Sub Module 1","content":"content"},"2":{"heading":"Sub Module 2","content":"content"}}}}}"""
 
 
 def construct_prompt_from_no_context_template(template: str, examples: str, user_prompt: str):
